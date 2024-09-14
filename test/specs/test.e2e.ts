7a35ed478366b285d5e209f08b6c0910deaec2eb
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-09-15 00:59:30 +0300

    add all 5 test case(need to update)

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -20,44 +20,39 @@ describe('My Login application', () => {
     // })
 
 
-    xit('Create new note', async () => {
+    it('Create new note', async () => {
         await browser.pause(5000);
 
-        // Замість пошуку з accessibility ID, тепер використовуємо XPath
         const sideMenuPath = "//android.widget.Button[@resource-id='com.samsung.android.app.notes:id/sign_in_button']";
         const sideMenuButton = await page.getElement(sideMenuPath)
-
-
-        console.log("sideMenuButton", sideMenuButton)
-
 
         await sideMenuButton.waitForDisplayed({ timeout: 10000 });
 
         await sideMenuButton.click();
 
-        await browser.pause(5000);
+        // // await browser.pause(5000);
 
         const penCreateIcon = await page.getElement("~Створити нотатку")
 
         await penCreateIcon.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const continueStylePageBtn = await page.getElement('//android.widget.Button[@resource-id="com.samsung.android.app.notes:id/pager_next_button"]')
 
         await continueStylePageBtn.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const doneSettingPageBtn = await page.getElement('//android.widget.Button[@resource-id="com.samsung.android.app.notes:id/pager_next_button"]')
 
         await doneSettingPageBtn.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
         const nameNote = await page.getElement('//android.widget.FrameLayout[@resource-id="com.samsung.android.app.notes:id/appbar_layout"]')
 
         await nameNote.click()
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
 
         await page.setElementInputValue('//android.widget.EditText[@resource-id="com.samsung.android.app.notes:id/comp_title_text"]', "qweqwe")
@@ -69,15 +64,15 @@ describe('My Login application', () => {
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const savedFolderName = await page.getElement('//android.widget.TextView[@resource-id="com.samsung.android.app.notes:id/title"]')
 
@@ -86,11 +81,9 @@ describe('My Login application', () => {
         expect(savedFolderNameText).toEqual('qweqwe');
 
         await browser.pause(5000);
-
-
     });
 
-    xit('Delete note', async () => {
+    it('Delete note', async () => {
         await browser.pause(5000);
 
         // // Замість пошуку з accessibility ID, тепер використовуємо XPath
@@ -123,31 +116,31 @@ describe('My Login application', () => {
 
         // await doneSettingPageBtn.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
         const nameNote = await page.getElement('//android.widget.FrameLayout[@resource-id="com.samsung.android.app.notes:id/appbar_layout"]')
 
         await nameNote.click()
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
 
         await page.setElementInputValue('//android.widget.EditText[@resource-id="com.samsung.android.app.notes:id/comp_title_text"]', "qweqwe12")
 
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const moveToTop = await page.getElement('~Перехід вгору')
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const savedFolderName = await page.getElement('//android.widget.TextView[@resource-id="com.samsung.android.app.notes:id/title"]')
 
@@ -157,79 +150,82 @@ describe('My Login application', () => {
 
         await savedFolderName.click()
 
-        await browser.pause(5000);
+        // // await browser.pause(5000);
 
         const otherOptionsBtn = await page.getElement('~Інші опції')
 
         await otherOptionsBtn.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const deleteIcon = await page.getElement('(//android.widget.ImageView[@resource-id="com.samsung.android.app.notes:id/item_button"])[6]')
 
         await deleteIcon.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const delCartLink = await page.getElement('//android.widget.Button[@resource-id="android:id/button1"]')
 
         await delCartLink.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await page.isElementDisplayed('//android.widget.TextView[@text="Усі нотатки"]')
 
         await browser.pause(2000);
-
-
     });
 
     it('Create a shared note', async () => {
 
-        // Замість пошуку з accessibility ID, тепер використовуємо XPath
-        const sideMenuPath = "//android.widget.Button[@resource-id='com.samsung.android.app.notes:id/sign_in_button']";
-        const sideMenuButton = await page.getElement(sideMenuPath)
+        // // // Замість пошуку з accessibility ID, тепер використовуємо XPath
+        // // const sideMenuPath = "//android.widget.Button[@resource-id='com.samsung.android.app.notes:id/sign_in_button']";
+        // // const sideMenuButton = await page.getElement(sideMenuPath)
 
 
-        console.log("sideMenuButton", sideMenuButton)
+        // // console.log("sideMenuButton", sideMenuButton)
 
 
-        await sideMenuButton.waitForDisplayed({ timeout: 10000 });
+        // // await sideMenuButton.waitForDisplayed({ timeout: 10000 });
 
-        await sideMenuButton.click();
+        // // await sideMenuButton.click();
 
-        await browser.pause(5000);
-        //-----------------------------------------------//
+        // // await browser.pause(5000);
+        // // //-----------------------------------------------//
 
         const shareNoteLink = await page.getElement('~Спільні нотатки')
 
         await shareNoteLink.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const penCreateIcon = await page.getElement("~Створити нотатку")
 
         await penCreateIcon.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const sendLinkBtn = await page.getElement('//android.view.ViewGroup[@resource-id="com.samsung.android.mobileservice:id/link_container"]')
 
         await sendLinkBtn.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const copyBtn = await page.getElement('(//android.widget.LinearLayout[@resource-id="android:id/sem_chooser_grid_item_view"])[5]')
 
         await copyBtn.click()
 
         await browser.pause(2000);
+        /////
+        await page.clickAndroidBackBtn()
+
+        /////
 
         const sharedNoteHeader = await page.getElement('//android.widget.TextView[@resource-id="com.samsung.android.app.notes:id/composer_toolbar_title"]')
 
         await sharedNoteHeader.click()
+        // await sharedNoteHeader.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await page.setElementInputValue('//android.widget.EditText[@resource-id="com.samsung.android.app.notes:id/comp_title_text"]', "My Shared Note")
 
@@ -239,15 +235,15 @@ describe('My Login application', () => {
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         await moveToTop.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const savedSharedName = await page.getElement('//android.widget.TextView[@resource-id="com.samsung.android.app.notes:id/title"]')
 
@@ -256,23 +252,77 @@ describe('My Login application', () => {
         expect(savedFolderNameText).toEqual("My Shared Note");
 
         await browser.pause(5000);
+    })
 
+    it('Delete a shared note', async () => {
+
+        // // Замість пошуку з accessibility ID, тепер використовуємо XPath
+        // const sideMenuPath = "//android.widget.Button[@resource-id='com.samsung.android.app.notes:id/sign_in_button']";
+        // const sideMenuButton = await page.getElement(sideMenuPath)
+
+
+        // console.log("sideMenuButton", sideMenuButton)
+
+
+        // await sideMenuButton.waitForDisplayed({ timeout: 10000 });
+
+        // await sideMenuButton.click();
+
+        // await browser.pause(5000);
+        // ///////////////////////////////////
+
+        // const shareNoteLink = await page.getElement('~Спільні нотатки')
+
+        // await shareNoteLink.click()
+
+        // await browser.pause(2000);
+        // //-----------------------------------------------//
+
+        const noteForDeleteLink = await page.getElement('//android.widget.TextView[@resource-id="com.samsung.android.app.notes:id/title" and @text="‎My Shared Note"]')
+
+        await noteForDeleteLink.click()
+
+        // // await browser.pause(2000)
+
+        const otherOptionsBtn = await page.getElement('~Інші опції')
+
+        await otherOptionsBtn.click()
+
+        // // await browser.pause(2000);
+
+        const delIcon = await page.getElement('(//android.widget.ImageView[@resource-id="com.samsung.android.app.notes:id/item_button"])[3]')
+
+        await delIcon.click()
+
+        // // await browser.pause(2000);
+
+        const delCartLink = await page.getElement('//android.widget.Button[@resource-id="android:id/button1"]')
+
+        await delCartLink.click()
+
+        // // await browser.pause(2000);
+
+        await page.isElementDisplayed('//android.widget.TextView[@text="Спільні нотатки"]')
+
+        await browser.pause(2000);
 
     })
 
-    xit('Check deleted notes', async () => {
+    it('Check deleted notes', async () => {
 
 
         const cartIcon = await page.getElement('~Кошик')
 
         await cartIcon.click()
 
-        await browser.pause(2000);
+        // // await browser.pause(2000);
 
         const countOfDeletedNotes = await page.getElement('//android.widget.TextView[@text="1 нотатка"]')
 
         const countOfDeletedNotesText = (await countOfDeletedNotes.getAttribute('text')).trim();
         expect(countOfDeletedNotesText).toEqual('1 нотатка');
+
+        await browser.pause(2000);
     });
 
 
